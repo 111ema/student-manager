082f275f92224eadfa400acb627a5fff35a7c3a5
lidia-bozin <lidia.bozin@student.upt.ro>
2026-04-01 17:26:34 +0300

    add

---
+++ src/main/java/Student.java
@@ -0,0 +1,44 @@
+package loose.oose.fis.lab.student.manager.model;
+
+import java.util.Objects;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+
+    public Student(double meanGrade, int age, String lastName, String firstName) {
+        this.meanGrade = meanGrade;
+        this.age = age;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Student student)) return false;
+        return age == student.age && Double.compare(meanGrade, student.meanGrade) == 0 && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age, meanGrade);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getMeanGrade() {
+        return meanGrade;
+    }
+}
